59e7afcc0555302906bcb1c591047d7a3406571f
Suraj Verma <surajverma@rentalcars.com>
2021-05-01 11:38:27 +0100

    add questions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("node app");
+const questions = [
+  {
+    type: "input",
+    message: "What is your full name?",
+    name: "fullName",
+  },
+  {
+    type: "input",
+    message: "Add a brief description about yourself",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What city do you reside in?",
+    name: "city",
+  },
+  {
+    type: "input",
+    message: "What country do you reside in?",
+    name: "country",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub URL?",
+    name: "githubUrl",
+  },
+  {
+    type: "input",
+    message: "What is your LinkedIn URL?",
+    name: "linkedinUrl",
+  },
+];
